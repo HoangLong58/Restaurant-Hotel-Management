b8ecfde4e51f302709acdaa01b4798e750586253
HoangLong58 <truonghoanglong58@gmail.com>
2022-09-16 14:47:05 +0700

    16/09/2022: Hoang Long init backend

---
+++ Luận văn 2022 CTU - BE/thesis/src/main/java/com/ctu/thesis/RestaurantHotelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ctu.thesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantHotelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantHotelManagementApplication.class, args);
+	}
+
+}
---
+++ Luận văn 2022 CTU - BE/thesis/src/test/java/com/ctu/thesis/RestaurantHotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctu.thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantHotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
